5640da611d9e72fbe358fc103fabf1cc8a9c529f
jairnet <jairnet5@gmail.com>
2020-02-29 13:35:58 -0500

    [FIX]change requirements

--- medica/settings.py
+++ medica/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #  En Local
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Deploy Heroku Configurations
 
